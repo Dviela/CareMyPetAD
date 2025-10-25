fe817f2cff7f8e58b89a748bc99f3e037b77cddb
Dviela <a19230@svalero.com>
2025-10-25 22:38:30 +0200

    Primer Commit

---
+++ src/main/java/com/svalero/CareMyPet/CareMyPetApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.CareMyPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareMyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareMyPetApplication.class, args);
+	}
+
+}

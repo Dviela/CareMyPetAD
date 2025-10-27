22ea79503e645553f80019201db9d946e4139656
Dviela <a19230@svalero.com>
2025-10-27 23:25:39 +0100

    Creado CaregiverController con metodos en blanco y añadida BD H2 en properties. Probado y estable

--- src/main/java/com/svalero/CareMyPet/controller/CaregiverController.java
+++ src/main/java/com/svalero/CareMyPet/controller/CaregiverController.java
@@ -1,4 +1,22 @@
 package com.svalero.CareMyPet.controller;
 
+import com.svalero.CareMyPet.domain.Caregiver;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CaregiverController {
+    @GetMapping("/caregiver")
+    public List<Caregiver> getAll() {
+        return null;
+    }
+
+    @PostMapping("/caregiver")
+    public void addCaregiver() {
+
+
+    }
 }
